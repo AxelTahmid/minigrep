d308f8c13204a61a904145ca2821d76799be7d79
Shahadat Hossain <axel.tahmid@gmail.com>
2024-02-24 21:20:55 +0600

    chore: refactor into modules

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // dbg!(args);
 
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let config = parse_config(&args);
 
-    println!("query is ===> {}", query);
-    println!("In file ===> {}", file_path);
+    println!("query is ===> {}", config.query);
+    println!("In file ===> {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should be able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read the file");
 
-    println!("with text: \n {}", contents)
+    println!("with text: \n {}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
