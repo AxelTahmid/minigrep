10e37591c2cd641b86e5fcbb27178480aff988a7
Shahadat Hossain <axel.tahmid@gmail.com>
2024-02-25 00:21:41 +0600

    chore: add search function with tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,37 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
 
-    println!("file content: \n {}", contents);
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    fs::read_to_string(config.file_path)?;
+    // let contents = fs::read_to_string(config.file_path)?;
+    // println!("file content: \n {}", contents);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
